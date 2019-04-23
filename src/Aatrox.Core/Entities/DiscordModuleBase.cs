8d476313338aa235846dfd32de3c8b3da058bcb6
Allan <allan.mercou@icloud.com>
2019-04-23 19:52:54 +0200

    Adds Respond methods to DiscordModuleBase. Adds an EmbedHelper class to easily create embeds.

--- src/Aatrox.Core/Entities/DiscordModuleBase.cs
+++ src/Aatrox.Core/Entities/DiscordModuleBase.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Aatrox.Core.Helpers;
+using Aatrox.Core.Services;
 using DSharpPlus.Entities;
 using Qmmands;
 
@@ -7,10 +9,43 @@ namespace Aatrox.Core.Entities
     public class DiscordModuleBase : ModuleBase<DiscordCommandContext>
     {
         public DatabaseContext DbContext => Context.DatabaseContext;
+        public InternationalizationService I18n => Context.I18n;
 
         public Task<DiscordMessage> RespondLocalizedAsync(string key, params object[] parameters)
         {
-            return Context.Channel.SendMessageAsync(Context.I18n.GetLocalization(key, parameters: parameters));
+            var localization = I18n.GetLocalization(key, parameters: parameters);
+
+            return Context.Channel.SendMessageAsync(localization);
+        }
+
+        public Task<DiscordMessage> RepondEmbedLocalizedAsync(string key, params object[] parameters)
+        {
+            var localization = I18n.GetLocalization(key, parameters: parameters);
+            var embed = EmbedHelper.New(Context, localization);
+
+            return Context.Channel.SendMessageAsync(embed: embed);
+        }
+
+        public Task<DiscordMessage> RespondAsync(string message)
+        {
+            return Context.Channel.SendMessageAsync(message);
+        }
+
+        public Task<DiscordMessage> RespondEmbedAsync(string message)
+        {
+            var embed = EmbedHelper.New(Context, message);
+
+            return Context.Channel.SendMessageAsync(embed: embed);
+        }
+
+        public Task<DiscordMessage> RespondAsync(string message, DiscordEmbed embed)
+        {
+            return Context.Channel.SendMessageAsync(message, embed: embed);
+        }
+
+        public Task<DiscordMessage> RespondAsync(DiscordEmbed embed)
+        {
+            return Context.Channel.SendMessageAsync(embed: embed);
         }
     }
 }
---
+++ src/Aatrox.Core/Helpers/EmbedHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Aatrox.Core.Entities;
+using DSharpPlus.Entities;
+
+namespace Aatrox.Core.Helpers
+{
+    public static class EmbedHelper
+    {
+        public static DiscordEmbedBuilder New(DiscordCommandContext ctx)
+        {
+            return new DiscordEmbedBuilder
+            {
+                Timestamp = DateTimeOffset.Now,
+                Footer = new DiscordEmbedBuilder.EmbedFooter
+                {
+                    Text = $"Executed by: {ctx.Member.DisplayName}#{ctx.Member.Discriminator}",
+                    IconUrl = ctx.Aatrox.AvatarUrl
+                },
+                Color = DiscordColor.DarkButNotBlack
+            };
+        }
+
+        public static DiscordEmbedBuilder New(DiscordCommandContext ctx, string description)
+        {
+            return new DiscordEmbedBuilder
+            {
+                Description = description,
+                Timestamp = DateTimeOffset.Now,
+                Footer = new DiscordEmbedBuilder.EmbedFooter
+                {
+                    Text = $"Executed by: {ctx.Member.DisplayName}#{ctx.Member.Discriminator}",
+                    IconUrl = ctx.Aatrox.AvatarUrl
+                },
+                Color = DiscordColor.DarkButNotBlack
+            };
+        }
+    }
+}
