b0a18d9fe9ef8b788c81ab6b0942f4d8c0ccb2f6
Allan Mercou <allan.mercou@icloud.com>
2019-07-09 16:05:30 +0200

    Renames Fun to Misc commands. Adds GuildAvailable event handling.

--- src/Aatrox.Core/Services/DiscordService.cs
+++ src/Aatrox.Core/Services/DiscordService.cs
@@ -13,11 +13,8 @@ namespace Aatrox.Core.Services
     public sealed class DiscordService
     {
         private readonly CommandService _commands;
-
         private readonly DiscordClient _client;
-
         private readonly LogService _logger;
-
         private readonly IServiceProvider _services;
 
         public DiscordService(CommandService commands, DiscordClient client, IServiceProvider services)
@@ -33,10 +30,18 @@ namespace Aatrox.Core.Services
             _client.MessageCreated += OnMessageCreatedAsync;
             _client.MessageUpdated += OnMessageUpdatedAsync;
             _client.Ready += OnReadyAsync;
+            _client.GuildAvailable += OnGuildAvailable;
 
             _commands.AddModules(assembly);
 
             await _client.ConnectAsync(status: UserStatus.DoNotDisturb);
+        }
+
+        private Task OnGuildAvailable(GuildCreateEventArgs e)
+        {
+            _logger.Info($"Guild available: {e.Guild.Name} ({e.Guild.Id})");
+
+            return Task.CompletedTask;
         }
 
         private async Task OnMessageCreatedAsync(MessageCreateEventArgs e)
@@ -84,7 +89,8 @@ namespace Aatrox.Core.Services
             var prefixes = new List<string>(ctx.DatabaseContext.Guild.Prefixes)
             {
                 $"<@{ctx.Aatrox.Id}> ",
-                $"<@!{ctx.Aatrox.Id}> "
+                $"<@!{ctx.Aatrox.Id}> ",
+                "Aa!"
             };
 
             if (!CommandUtilities.HasAnyPrefix(ctx.Message.Content, prefixes, StringComparison.OrdinalIgnoreCase, out var prefix, out var content))
--- src/Aatrox/Modules/MiscCommands.cs
+++ src/Aatrox/Modules/MiscCommands.cs
@@ -4,13 +4,21 @@ using Qmmands;
 
 namespace Aatrox.Modules
 {
-    public sealed class FunCommands : DiscordModuleBase
+    [Name("Misc")]
+    public sealed class MiscCommands : DiscordModuleBase
     {
         [Command("Ping")]
         [Description("Shows the current websocket's latency.")]
         public Task PingAsync()
         {
             return RespondLocalizedAsync("ping", Context.Client.Ping);
+        }
+
+        [Command("Invite")]
+        [Description("Shows the bot's invitation url.")]
+        public Task InviteAsync()
+        {
+            return RespondLocalizedAsync("invite", Context.Aatrox.Id, 8);
         }
     }
 }
