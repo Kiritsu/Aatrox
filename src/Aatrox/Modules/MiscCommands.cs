e5c865315fd95c45b8112d75ad2b81f72cbc3ac2
Allan <allan.mercou@icloud.com>
2019-11-03 01:20:11 +0100

    Better ping command

--- src/Aatrox/Modules/MiscCommands.cs
+++ src/Aatrox/Modules/MiscCommands.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Aatrox.Core.Entities;
+using Aatrox.Core.Interfaces;
+using Disqord;
 using Qmmands;
 
 namespace Aatrox.Modules
@@ -8,13 +12,49 @@ namespace Aatrox.Modules
     [Name("Misc")]
     public sealed class MiscCommands : AatroxModuleBase
     {
+        private readonly AatroxConfiguration _configuration;
+
+        public MiscCommands(IAatroxConfigurationProvider config)
+        {
+            _configuration = config.GetConfiguration();
+        }
+
         [Command("Ping")]
         [Description("Shows the current websocket's latency.")]
-        public Task PingAsync()
+        public async Task PingAsync(string host = "8.8.8.8")
         {
-            return RespondLocalizedAsync("ping", Context.Client.Latency.HasValue 
-                ? Math.Round(Context.Client.Latency.Value.TotalMilliseconds, 2) 
-                : -42);
+            long distant = 0;
+            using var ping = new Ping();
+
+            try
+            {
+                distant = (await ping.SendPingAsync(host)).RoundtripTime;
+            }
+            catch (Exception)
+            {
+                host += ": timed out";
+            }
+
+            var emb = new LocalEmbedBuilder()
+                .WithColor(_configuration.EmbedColor)
+                .WithDescription($":heart:  |  {(Context.Client.Latency.HasValue ? Math.Round(Context.Client.Latency.Value.TotalMilliseconds, 2): -42)}ms " +
+                    $"\n:earth_americas:  |  {distant}ms ({host})")
+                .WithTitle("Current latency : (websocket, host, messages)");
+
+            var sw = Stopwatch.StartNew();
+            var message = await RespondAsync(emb.Build()).ConfigureAwait(false);
+            sw.Stop();
+
+            emb.Description += $"\n:e_mail:  |  {sw.ElapsedMilliseconds}ms";
+
+            for (var i = 0; i < 5; i++)
+            {
+                sw.Restart();
+                await message.ModifyAsync(x => x.Embed = emb.Build()).ConfigureAwait(false);
+                sw.Stop();
+
+                emb.Description += $", {sw.ElapsedMilliseconds}ms";
+            }
         }
 
         [Command("Invite")]
