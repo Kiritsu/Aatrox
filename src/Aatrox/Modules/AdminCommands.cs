636c0a57507f8b0b1906be9a35dc10820329decb
Allan <allan.mercou@icloud.com>
2020-01-19 16:33:51 +0100

    Adds command to reload i18n

---
+++ src/Aatrox/Modules/AdminCommands.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Aatrox.Core.Entities;
+using Qmmands;
+
+namespace Aatrox.Modules
+{
+    [Name("Admin"), Group("Admin")]
+    public sealed class AdminCommands : AatroxModuleBase
+    {
+        [Command("ReloadLanguage")]
+        public async Task ReloadLanguageAsync()
+        {
+            await MultiLanguage.SetupAsync();
+            await RespondAsync(":ok_hand:");
+        }
+    }
+}
